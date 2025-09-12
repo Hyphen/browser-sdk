38de4fbb8dad6f420f00fb0e043d93a2c2d01e3c
Jared Wray <me@jaredwray.com>
2025-09-12 13:20:15 -0700

    feat: adding in the base toggle class and @cacheable/net

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
+import { type CacheableNet, Net } from "@cacheable/net";
 import { Hookified } from "hookified";
 
 export class Toggle extends Hookified {
-	// biome-ignore lint/complexity/noUselessConstructor: init
-	constructor() {
-		super();
+	private _net = new Net();
+
+	public get net(): CacheableNet {
+		return this._net;
+	}
+
+	public set net(value: CacheableNet) {
+		this._net = value;
 	}
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,13 +1,47 @@
+import { Net } from "@cacheable/net";
 import { describe, expect, test } from "vitest";
 import { Toggle } from "../src/index.js";
 
 describe("Hyphen sdk", () => {
 	test("should create an instance of Toggle", () => {
-		const toggle = new Toggle({
-			applicationId: "my-app",
-			publicApiKey: "public_my-public-key",
-			environment: "development",
-		});
+		const toggle = new Toggle();
 		expect(toggle).toBeInstanceOf(Toggle);
+	});
+
+	describe("net property", () => {
+		test("should return a Net instance by default", () => {
+			const toggle = new Toggle();
+			expect(toggle.net).toBeInstanceOf(Net);
+		});
+
+		test("should allow getting the net property", () => {
+			const toggle = new Toggle();
+			const net = toggle.net;
+			expect(net).toBeDefined();
+			expect(typeof net).toBe("object");
+		});
+
+		test("should allow setting the net property", () => {
+			const toggle = new Toggle();
+			const customNet = new Net();
+			toggle.net = customNet;
+			expect(toggle.net).toBe(customNet);
+		});
+
+		test("should maintain the same net instance when accessed multiple times", () => {
+			const toggle = new Toggle();
+			const net1 = toggle.net;
+			const net2 = toggle.net;
+			expect(net1).toBe(net2);
+		});
+
+		test("should update net property when set to a new instance", () => {
+			const toggle = new Toggle();
+			const originalNet = toggle.net;
+			const newNet = new Net();
+			toggle.net = newNet;
+			expect(toggle.net).not.toBe(originalNet);
+			expect(toggle.net).toBe(newNet);
+		});
 	});
 });
